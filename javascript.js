753be3a852eaddcd4ca980ca39ec6ccaec42db1f
Bart Wagener <bartwagener@gmail.com>
2024-10-11 09:56:33 +0200

    Function for random computer selection finished

--- javascript.js
+++ javascript.js
@@ -1,5 +1,18 @@
+//This function sets computer choice, randomly selecting a number between 1 and 3
+
+function getComputerChoice() {
+    const randomNumber =  Math.floor(Math.random() *3 +1)
+
+    if (randomNumber ===1) {
+        return "Rock";
+    } else if (randomNumber === 2) {
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    }
+    
+}
 
 
-const test = "Hello"
-
-console.log(test)
+console.log(getComputerChoice())
