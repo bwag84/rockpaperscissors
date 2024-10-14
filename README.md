8aafebe357df7af5a6e98d921aa4fa767e8379ad
Bart Wagener <bartwagener@gmail.com>
2024-10-14 10:42:55 +0200

    Finished. Moved the playRound into playGame Required moving human and com choice inside the loop

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rockpaperscissors
 Rock paper scissors game in JavaScript for the Odin project
+
+
--- javascript.js
+++ javascript.js
@@ -26,7 +26,7 @@ function getHumanChoice() {
     const validChoices = ["rock", "paper", "scissors"]; //an array with the correct answers, lowerCase for validating
 
     while (true){
-        userInput = prompt ("Please write Rock, paper or scissors").toLowerCase(); //converts the prompt to lowerCase which makes validating easier
+        userInput = prompt ("Please write rock, paper or scissors").toLowerCase(); //converts the prompt to lowerCase which makes validating easier
         
         if (validChoices.includes(userInput)){
             return userInput; //returns the valid input
@@ -39,10 +39,10 @@ function getHumanChoice() {
 }
 
 
-const humanSelection = getHumanChoice(); //does this run the prompt a second time?
+const humanSelection = getHumanChoice(); 
 const computerSelection = getComputerChoice();
 
-function playRound(humanChoice, computerChoice){ //the naming of the arguments is arbitrary. It could be a, b. But this is an effort to be more clear, although it confuses me a little.
+function playRound(humanChoice, computerChoice){ //the naming of the arguments is arbitrary. It could be a, b. But this is readable
     if ((humanChoice === "rock" && computerChoice === "scissors") ||
         (humanChoice === "paper" && computerChoice === "rock") ||
         (humanChoice === "scissors" && computerChoice === "paper")) {
@@ -60,16 +60,54 @@ function playRound(humanChoice, computerChoice){ //the naming of the arguments i
         }
 
         console.log("Score: " +  "Humans " + humanScore, "Computer "+ computerScore)
+        
 
 }
 
+// Function to play a single round of the game
+function playRound(humanChoice, computerChoice) {
+    if ((humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")) {
+        humanScore++;
+        console.log("You Win!");
+    } else if ((computerChoice === "rock" && humanChoice === "scissors") ||
+               (computerChoice === "paper" && humanChoice === "rock") ||
+               (computerChoice === "scissors" && humanChoice === "paper")) {
+        computerScore++;
+        console.log("You Lose!");
+    } else {
+        console.log("It's a draw!");
+    }
+
+    console.log("Score: Humans " + humanScore + ", Computer " + computerScore);
+}
+
+// Function to play the game (best of 5 rounds)
+function playGame() {
+    for (let rounds = 0; rounds < 5; rounds++) {
+        console.log("Round " + (rounds + 1));
+        const humanChoice = getHumanChoice(); // Get human choice for each round
+        const computerChoice = getComputerChoice(); // Get computer choice for each round
+        playRound(humanChoice, computerChoice); // Play the round with the chosen values
+    }
+
+    console.log("Final Score: Humans " + humanScore + ", Computer " + computerScore);
+    if (humanScore > computerScore) {
+        console.log("Congratulations! You won the game.");
+    } else if (computerScore > humanScore) {
+        console.log("Computer wins the game. Better luck next time.");
+    } else {
+        console.log("It's a tie overall!");
+    }
+}
 
 
-playRound(humanSelection, computerSelection);
-
-
-
+playGame();
 
 // Testing results in the browser console
-console.log("the computer chose " + getComputerChoice())
+console.log("the computer chose " + computerSelection)
+
+
+
 //console.log("the human chose " + getHumanChoice)
